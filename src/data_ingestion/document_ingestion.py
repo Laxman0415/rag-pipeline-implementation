5d3c781321c37f3c485a87e145b3d3d4211c4d5c
Laxman Singh <singhlaxmandd@gmail.com>
2025-05-23 21:31:42 +0530

    May 23,2025 | 09:31:00 PM IST | Data Ingestion | Added Data Ingestion Changes

---
+++ src/data_ingestion/document_ingestion.py
@@ -0,0 +1,114 @@
+import os
+import sys
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+
+from utility.custom_logger import logger
+from utility.custom_exception import CustomException
+
+from utility.utils import load_metadata, get_google_embedding_model , save_metadata 
+
+
+
+class DocumentIngestion:
+
+    METADATA_FILE = "config/faiss_index_metadata.json"
+    INDEX_DIR = "faiss_index"  # Directory to store multiple FAISS indexes
+
+    def __init__(self,web_doc_url):
+        logger.info(f"Document URL is : {web_doc_url=}")
+        self.web_doc_url = web_doc_url
+           
+    def get_raw_document(self):
+        """        
+        """
+        try:
+            
+            logger.info("IN Load")
+            loader = UnstructuredURLLoader(urls=[self.web_doc_url])
+            # Load the document
+            documents = loader.load()
+            logger.info("After loader.load()")
+            return documents
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def document_chunking(self):
+        """
+            # Chunking
+            # If Chunking is not require , we can directly do and store the embeddings
+            # Chunked Doc , Just a Way to Arrange the metadata 
+        """
+        try:
+
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap =200)
+            raw_document = self.get_raw_document()
+            chunked_data = text_splitter.split_documents(raw_document)
+            return chunked_data
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    @staticmethod    
+    def vector_store_save_document(chunked_data,embedding,index_path):
+        """
+        # Configuring VECTOR DB
+        # WE are using FAISS (In Memory Database) Facebook AI Similarity Search 
+
+        # It was a mathematical algorithm to perform indexing on top of data
+
+        # FAISS.from_documents(documents=docs,embedding=hf_embeddings)
+        """
+        try:
+            vector_store = FAISS.from_documents(documents=chunked_data,embedding=embedding)
+            # Save index for later use
+            vector_store.save_local(index_path)            
+            # retriever = vector_store.as_retriever()
+            # return vector_store,retriever
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+
+    def run_data_ingestion(self):
+        try:
+            pre_stored_metadata_config =  load_metadata(DocumentIngestion.METADATA_FILE)
+            pre_stored_knowledge_base_flag = False
+            # Generating the unique index filename for this Knowledge Base
+            index_filename_data = pre_stored_metadata_config.get(self.web_doc_url,{}) 
+            index_filename = index_filename_data.get("vector_store_index",f"{len(pre_stored_metadata_config)}.faiss")  # Assigning a new index if not found
+            index_path = os.path.join(DocumentIngestion.INDEX_DIR, index_filename)
+            logger.info(f"{index_path=}")
+
+            if not os.path.exists(DocumentIngestion.INDEX_DIR):
+                os.makedirs(DocumentIngestion.INDEX_DIR)  # Creating a directory as it doesn't exist
+
+            if os.path.exists(index_path):
+                logger.info(f"Loading existing FAISS index for {self.web_doc_url}...")
+                pre_stored_knowledge_base_flag = True
+                logger.info("Knowledge Base is Already Ingested in DB")
+                # vector_store = FAISS.load_local(index_path, embedding,allow_dangerous_deserialization=True)
+                # retriever = vector_store.as_retriever()
+            else:
+                logger.info(f"Generating new FAISS index for newly queried Book : {self.web_doc_url}...")
+                logger.info("Starting the Process from Scratch")
+                queried_web_doc_metadata = {}
+                queried_web_doc_metadata["vector_store_index"] = index_filename
+                chunked_data = self.document_chunking()
+                google_embedding = get_google_embedding_model()
+                DocumentIngestion.vector_store_save_document(chunked_data, google_embedding,index_path)
+                
+                # Updating the metadata with the new mapping and Queried New Web Doc Metadata
+                pre_stored_metadata_config[self.web_doc_url] = queried_web_doc_metadata
+                logger.info(f"{pre_stored_metadata_config=}")
+                save_metadata(pre_stored_metadata_config,DocumentIngestion.METADATA_FILE)
+            
+            return pre_stored_knowledge_base_flag
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+if __name__ == "__main__":
+    document_ingestion = DocumentIngestion("https://www.gutenberg.org/cache/epub/13421/pg13421.txt")
+    document_ingestion.get_raw_document()
